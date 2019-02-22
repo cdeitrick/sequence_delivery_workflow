3a426740fae92f5993099f19c14c396f4731b38a
Unknown <cld100@pitt.edu>
2019-02-22 15:57:11 -0500

    updated with migs credentials

--- boxapi.py
+++ boxapi.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 from typing import Dict
-import re
+
 from boxsdk import Client, OAuth2
 
 TOKEN_STORAGE = Path.home() / "box_tokens.json"
@@ -11,8 +11,21 @@ DEVELOPER_TOKEN = "1popFVeRf3KpCBNCZkWviCPLazVMGXyS"
 
 def get_credentials() -> Dict[str, str]:
 	""" Retrieves the client ID and client secret that have been saved previously."""
-	filename = Path.home() / "box_credentials.json"
-	data = json.loads(filename.read_text())
+	beagle_path = Path("/home/data/boxapi.txt")
+	if beagle_path.exists:
+		contents = [i.strip() for i in beagle_path.read_text().split("\n") if i]
+		key, secret, folder_id = contents
+		data = {
+			"boxAppSettings": {
+				"clientID":     key,
+				"clientSecret": secret
+			}
+		}
+		global PARENT_FOLDER_ID
+		PARENT_FOLDER_ID = folder_id
+	else:
+		filename = Path.home() / "box_credentials.json"
+		data = json.loads(filename.read_text())
 
 	return data['boxAppSettings']
 
@@ -55,12 +68,12 @@ def authorize_with_login() -> OAuth2:
 	auth_request_url, csrf_token = authorization.get_authorization_url(redirect_url)
 	print("Enter this URL into a browser to authorize the app.")
 	print(auth_request_url)
-	#https://migsapp.com/auth?state=box_csrf_token_mlbRGxXCho5Z1NDg&code=Bu2YNuNeCa1g5mThmka4vjRZb58LBJkL
+	# https://migsapp.com/auth?state=box_csrf_token_mlbRGxXCho5Z1NDg&code=Bu2YNuNeCa1g5mThmka4vjRZb58LBJkL
 	access_code_url = input("Please enter the access code url: ")
-	#access_code = re.match("code=(.*)$", access_code_url.strip())
-	#if access_code:
+	# access_code = re.match("code=(.*)$", access_code_url.strip())
+	# if access_code:
 	#	access_code = access_code.group(0)
-	#else:
+	# else:
 	#	message = f"invalid access url: {access_code_url}"
 	#	print(access_code)
 	#	raise ValueError(message)
@@ -73,7 +86,7 @@ def authorize_with_login() -> OAuth2:
 	return authorization
 
 
-#CLIENT = Client(authorize_with_oauth2())
+# CLIENT = Client(authorize_with_oauth2())
 CLIENT = Client(authorize_with_login())
 FOLDER = CLIENT.folder(PARENT_FOLDER_ID).get(fields = None, etag = None)
 if __name__ == "__main__":
